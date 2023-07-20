17744b58f575df61593f3075588b8268d1ba1190
Thrawien <mad_alchemist@hotmail.com>
2023-07-20 13:42:13 +0100

    Guess the state game

--- day_25_us_states_game/main.py
+++ day_25_us_states_game/main.py
@@ -1,23 +1,37 @@
 import turtle, pandas
 
+
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
-
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another State's name?").title()
+guessed_states = []
 
-#check if guess is among the 50 states
-data = pandas.read_csv("50_states.csv")
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="What's another State's name?").title()
 
-#write the correct guesses onto the map
+    #check if guess is among the 50 states
+    data = pandas.read_csv("50_states.csv")
+    state_list = data["state"].to_list()
 
-#use a loop to allow the user to keep guessing
-
-#record the guesses in a list
-
-#keep track of the score
-
-turtle.mainloop()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in state_list:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn")
+        break
+    if answer_state in state_list:
+        guessed_states.append(answer_state)
+        # write the correct guesses onto the map
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        # pull out the row where the state is equal to the answer state
+        state_data = data[data.state == answer_state]
+        #tap into the data attributes using the names of the columns, in this case x and y
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
