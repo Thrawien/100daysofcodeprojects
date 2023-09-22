66648ad57b6c12b3b6d6092fe163d3d473683260
Thrawien <mad_alchemist@hotmail.com>
2023-09-22 14:36:25 +0100

    Day 27 completed and refined

--- day_27/Miles_to_Km.py
+++ day_27/Miles_to_Km.py
@@ -1,10 +1,10 @@
 from tkinter import *
-FONT = "Arial", 24, "bold"
+FONT = "Arial", 21, "bold"
 
 def button_clicked():
-    query = int(miles_input.get())
-    answer = str(query * 1.6)
-    answer_label.config(text=answer)
+    miles = float(miles_input.get())
+    kilometer = round(miles * 1.609)
+    answer_label.config(text=f"{kilometer}")
 
 
 window = Tk()
@@ -16,25 +16,31 @@ window.config(padx=20, pady=20)
 
 equals_label = Label(text="is equal to", font=(FONT))
 equals_label.grid(column=0, row= 1)
+equals_label.config(padx=20, pady=20)
 
 miles_label = Label(text="Miles", font=(FONT))
 miles_label.grid(column=2, row=0)
+miles_label.config(padx=20, pady=20)
 
 km_label = Label(text="Km", font=(FONT))
 km_label.grid(column=2, row=1)
+km_label.config(padx=20, pady=20)
 
 answer_label = Label(text="0", font=(FONT))
 answer_label.grid(column=1, row=1)
+answer_label.config(padx=20, pady=20)
 
 # Button
 
 button = Button(text="Calculate", command=button_clicked)
 button.grid(column=1, row=2)
+button.config(padx=20, pady=20)
 
 # Entry
 
 miles_input = Entry(width=10)
 miles_input.grid(column=1, row=0)
+
 
 
 
