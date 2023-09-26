6ac143417da34edddf90d174a21df9dd7b881176
Thrawien <mad_alchemist@hotmail.com>
2023-09-26 14:02:27 +0100

    day_28_pomodoro completed and checked

--- day_27/Miles_to_Km.py
+++ day_27/Miles_to_Km.py
@@ -1,5 +1,7 @@
 from tkinter import *
+
 FONT = "Arial", 21, "bold"
+
 
 def button_clicked():
     miles = float(miles_input.get())
@@ -14,19 +16,19 @@ window.config(padx=20, pady=20)
 
 # Label
 
-equals_label = Label(text="is equal to", font=(FONT))
-equals_label.grid(column=0, row= 1)
+equals_label = Label(text="is equal to", font=FONT)
+equals_label.grid(column=0, row=1)
 equals_label.config(padx=20, pady=20)
 
-miles_label = Label(text="Miles", font=(FONT))
+miles_label = Label(text="Miles", font=FONT)
 miles_label.grid(column=2, row=0)
 miles_label.config(padx=20, pady=20)
 
-km_label = Label(text="Km", font=(FONT))
+km_label = Label(text="Km", font=FONT)
 km_label.grid(column=2, row=1)
 km_label.config(padx=20, pady=20)
 
-answer_label = Label(text="0", font=(FONT))
+answer_label = Label(text="0", font=FONT)
 answer_label.grid(column=1, row=1)
 answer_label.config(padx=20, pady=20)
 
@@ -40,9 +42,5 @@ button.config(padx=20, pady=20)
 
 miles_input = Entry(width=10)
 miles_input.grid(column=1, row=0)
-
-
-
-
 
 window.mainloop()
